719947baee0c56d4ab00c329917cacda9f360d3d
maazpendari <240911885+Maazpendari01@users.noreply.github.com>
2025-11-06 12:23:16 +0530

    changed import to chorma library

--- backend/app/services/knowledge_base.py
+++ backend/app/services/knowledge_base.py
@@ -1,4 +1,4 @@
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings  # FREE embeddings!
 import json
 
